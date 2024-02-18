f3e2977287b88f54781489b43ede1e9e951984a8
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-02-18 13:15:26 +0200

    stuff

--- lib/vkpm2/adapters/config.rb
+++ lib/vkpm2/adapters/config.rb
@@ -3,8 +3,16 @@
 module VKPM2
   module Adapters
     class Config
+      ABSOLUTE_DIR = File.join(Dir.home, '.config', 'vkpm').freeze
+      FILE = 'config.yaml'
+
       def initialize(client: Clients::Config.new)
         @client = client
+
+        client.append_path(ABSOLUTE_DIR)
+        config.filename = FILE
+
+        config.write unless config.exist?
       end
 
       private
