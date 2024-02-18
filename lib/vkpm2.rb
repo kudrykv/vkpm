438fcdcde1075612e99d6b44e81d5e694908f630
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-02-18 12:30:49 +0200

    stuff

--- lib/vkpm2.rb
+++ lib/vkpm2.rb
@@ -2,7 +2,13 @@
 
 require_relative 'vkpm2/version'
 
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect(
+  'vkpm2' => 'VKPM2',
+  'cli' => 'CLI'
+)
+loader.setup
+
 module Vkpm2
   class Error < StandardError; end
-  # Your code goes here...
 end
